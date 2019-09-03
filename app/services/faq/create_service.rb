141fd601a09c172039a42cfc21b09cad51a290cb
cauecjm <cauecjm@gmail.com>
2019-09-03 20:17:01 -0300

    app/services/faq: add create_service.rb, list_service.rb e remove_service.rb

---
+++ app/services/faq/create_service.rb
@@ -0,0 +1,25 @@
+module FaqModule
+  class CreateService
+
+    def initialize(params)
+      @question = params[:question]
+      @answer   = params[:answer]
+      @hashtags = params[:hashtags]
+    end
+
+    def perform
+      return 'Hashtag é obrigatório' if @hashtags.nil?
+
+      Faq.transaction do
+        faq = Faq.create(question: @question, answer: @answer)
+
+        @hashtags.split(/[\s,]+/).each do |hashtag|
+          faq.hashtags << Hashtag.find_or_create_by(name: hashtag)
+        end
+      end
+
+      'Criado com sucesso'
+    end
+
+  end
+end
---
+++ app/services/faq/remove_service.rb
@@ -0,0 +1,27 @@
+module FaqModule
+  class RemoveService
+
+    def initialize(params)
+      @params = params
+      @id     = params[:id]
+    end
+
+    def perform
+      faq = Faq.where(id: @id).last
+
+      return 'Questão inválida, verifique o Id' if faq == nil
+
+      Faq.transaction do
+        # Deleta as tags associadas que não estejam associadas a outros faqs
+        faq.hashtags.each do |h|
+          h.delete if h.faqs.count <= 1
+        end
+
+        faq.delete
+
+        'Deletado com sucesso'
+      end
+    end
+
+  end
+end
