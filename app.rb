c7127c379cd122566d7d0ac14b83ef4b1fcd2fe7
cauecjm <cauecjm@gmail.com>
2019-09-10 13:42:10 -0300

    app.rb: rm chave fulfillmentMessages

--- app.rb
+++ app.rb
@@ -27,12 +27,6 @@ class App < Sinatra::Base
     content_type :json, charset: 'utf-8'
     {
       'fulfillmentText': response,
-      'fulfillmentMessages': [
-        {
-          'text': [ response ]
-        }
-      ],
-      'source': 'https://nazusoft-bitchatbot.herokuapp.com',
       'payload': {
         'telegram': {
           'text': response
