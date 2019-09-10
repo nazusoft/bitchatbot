5f567a6373de087dc900670d201c047d956276d2
cauecjm <cauecjm@gmail.com>
2019-09-10 17:38:39 -0300

    help_service.rb: corringindo layout

--- app/services/help_service.rb
+++ app/services/help_service.rb
@@ -2,25 +2,19 @@ class HelpService
 
   # quando utiliza 'self' antes do nome do método, não precisa utilizar 'new'
   def self.perform
-    response  = '*Meus comandos*    '
-
-    response += '- help  '
-    response += '_Lista de comandos que eu conheço_    '
-
-    response += '- Adicione ao Faq  '
-    response += '__Adiciona uma nova questão ao Faq__    '
-
-    response += '- Remover ID  '
-    response += '__Remove uma questão baseada no ID dela__    '
-
-    response += '- O que você sabe sobre X  '
-    response += '__Pesquisa por palavra na lista de perguntas e respostas__  '
-
-    response += '- Pesquise a hashtag X  '
-    response += '__Lista as perguntas e respostas com aquela hashtag__  '
-
-    response += '- Perguntas e Respostas  '
-    response += '__Mostra a lista de perguntas e respostas__  '
+    response  = '*Meus comandos*    
+      - help  
+      _Lista de comandos que eu conheço_    
+      - Adicione ao Faq  
+      _Adiciona uma nova questão ao Faq_    
+      - Remover ID  
+      _Remove uma questão baseada no ID dela_    
+      - O que você sabe sobre X  
+      _Pesquisa por palavra na lista de perguntas e respostas_  
+      - Pesquise a hashtag X  
+      _Lista as perguntas e respostas com aquela hashtag_  
+      - Perguntas e Respostas  
+      _Mostra a lista de perguntas e respostas_  '
 
     response
   end
