67527241ec4d0f7e964e1b138a79663cf722f6f5
cauecjm <cauecjm@gmail.com>
2019-09-10 12:12:22 -0300

    help_service.rb: refatorando texto - usando tags html

--- app/services/help_service.rb
+++ app/services/help_service.rb
@@ -2,19 +2,25 @@ class HelpService
 
   # quando utiliza 'self' antes do nome do método, não precisa utilizar 'new'
   def self.perform
-    response  = '*Meus comandos*\n\n'
-    response += 'help\n'
-    response += '`Lista de comandos que eu conheço`\n\n'
-    response += 'Adicione ao Faq\n'
-    response += '`Adiciona uma nova questão ao Faq`\n\n'
-    response += 'Remova ID\n'
-    response += '`Remove uma questão baseada no ID dela`\n\n'
-    response += 'O que você sabe sobre X\n'
-    response += '`Pesquisa por palavra na lista de perguntas e respostas`\n\n'
-    response += 'Pesquise a hashtag X\n'
-    response += '`Lista as perguntas e respostas com aquela hashtag`\n\n'
-    response += 'Perguntas e Respostas\n'
-    response += '`Mostra a lista de perguntas e respostas`\n\n'
+    response  = '<b>Meus comandos</b></br></br>'
+
+    response += '- help</br>'
+    response += '<i>Lista de comandos.</i></br></br>'
+
+    response += '- Adicione ao Faq</br>'
+    response += '<i>Adiciona uma nova questão ao Faq.</i></br></br>'
+
+    response += '- Remova ID</br>'
+    response += '<i>Remove uma questão baseada no ID.</i></br></br>'
+
+    response += '- O que você sabe sobre X</br>'
+    response += '<i>Pesquisa por palavra na lista de perguntas e respostas</i></br></br></br>'
+
+    response += '- Pesquise a hashtag X</br>'
+    response += '<i>Lista as perguntas e respostas com aquela hashtag</i></br></br>'
+
+    response += '- Perguntas e Respostas</br>'
+    response += '<i>Mostra a lista de perguntas e respostas</i></br></br>'
 
     response
   end
