1dac62df40b8385dd2a98487cf861240825924c8
cauecjm <cauecjm@gmail.com>
2019-09-10 22:47:04 -0300

    app.rb|help_service.rb: alterando layout usando html

--- app/services/help_service.rb
+++ app/services/help_service.rb
@@ -3,25 +3,25 @@ class HelpService
   # quando utiliza 'self' antes do nome do método, não precisa utilizar 'new'
   def self.perform
     response  = 
-      '*Meus comandos*
+      '<b>Comandos:</b></br></br>
 
-      - help
-      _Lista de comandos que eu conheço_
+      - help</br>
+      <i>Lista de comandos que eu conheço</i></br></br>
 
-      - Adicione ao Faq
-      _Adiciona uma nova questão ao Faq_
+      - Adicione ao Faq</br>
+      <i>Adiciona uma nova questão ao Faq</i></br></br>
 
-      - Remover ID
-      _Remove uma questão baseada no ID dela_
+      - Remover ID</br>
+      <i>Remove uma questão baseada no ID dela</i></br></br>
 
-      - O que você sabe sobre X
-      _Pesquisa por palavra na lista de perguntas e respostas_
+      - O que você sabe sobre X</br>
+      <i>Pesquisa por palavra na lista de perguntas e respostas</i></br></br>
 
-      - Pesquise a hashtag X
-      _Lista as perguntas e respostas com aquela hashtag_
+      - Pesquise a hashtag X</br>
+      <i>Lista as perguntas e respostas com aquela hashtag</i></br></br>
 
-      - Perguntas e Respostas
-      _Mostra a lista de perguntas e respostas_'
+      - Perguntas e Respostas</br>
+      <i>Mostra a lista de perguntas e respostas</i></br></br>'
 
     response
   end
