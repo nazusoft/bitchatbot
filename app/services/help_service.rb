3d42a06ea72f6137cc23b70c9d335b5d2c384742
cauecjm <cauecjm@gmail.com>
2019-09-10 23:08:17 -0300

    help_service.rb: rm tags html </br>

--- app/services/help_service.rb
+++ app/services/help_service.rb
@@ -3,25 +3,25 @@ class HelpService
   # quando utiliza 'self' antes do nome do método, não precisa utilizar 'new'
   def self.perform
     response  = 
-      '<b>Comandos:</b></br></br>
+      '<b>Comandos:</b>
 
       <code>help</code>
-      <i>Lista de comandos que eu conheço</i></br></br>
+      <i>Lista de comandos que eu conheço</i>
 
       <code>Adicione ao Faq</code>
-      <i>Adiciona uma nova questão ao Faq</i></br></br>
+      <i>Adiciona uma nova questão ao Faq</i>
 
       <code>Remover ID</code>
-      <i>Remove uma questão baseada no ID dela</i></br></br>
+      <i>Remove uma questão baseada no ID dela</i>
 
       <code>O que você sabe sobre X</code>
-      <i>Pesquisa por palavra na lista de perguntas e respostas</i></br></br>
+      <i>Pesquisa por palavra na lista de perguntas e respostas</i>
 
       <code>Pesquise a hashtag X</code>
-      <i>Lista as perguntas e respostas com aquela hashtag</i></br></br>
+      <i>Lista as perguntas e respostas com aquela hashtag</i>
 
       <code>Perguntas e Respostas</code>
-      <i>Mostra a lista de perguntas e respostas</i></br></br>'
+      <i>Mostra a lista de perguntas e respostas</i>'
 
     response
   end
